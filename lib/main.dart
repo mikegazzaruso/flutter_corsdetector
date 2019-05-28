b5e1727fb3038315337ca7aa6a5e8f6802365fc7
Mike Gazzaruso <mike@devoloop.com>
2019-05-28 21:37:37 +0200

    Implemented Utterance and CORS Checking

--- lib/main.dart
+++ lib/main.dart
@@ -1,61 +1,150 @@
 import 'package:flutter_web/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:html';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'CORS Detector',
+      theme: ThemeData.dark(),
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+class MyHomePage extends StatefulWidget {
+  @override
+  State<MyHomePage> createState() {
+    return _MyHomePage();
+  }
+}
 
-  final String title;
+class _MyHomePage extends State<MyHomePage> {
+  final _myController = TextEditingController();
+  bool _canVerify;
+  bool _isBusy;
+  String _message;
+
+  @override
+  void initState() {
+    _canVerify = false;
+    _isBusy = false;
+    _message = '';
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        title: Text(title),
+        title: Text('Dead or Alive - Made with Flutter'),
+        centerTitle: true,
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (choose the "Toggle Debug Paint" action
-          // from the Flutter Inspector in Android Studio, or the "Toggle Debug
-          // Paint" command in Visual Studio Code) to see the wireframe for each
-          // widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
-              'Hello, World!',
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Text(
+                '',
+                style: TextStyle(fontSize: 24.0),
+              ),
+            ),
+            Expanded(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.all(20.0),
+                      child: TextField(
+                        onChanged: (value) => _handleOnChanged(value),
+                        controller: _myController,
+                        autofocus: true,
+                        decoration: const InputDecoration(
+                          helperText: "Enter a FQDN or an URL",
+                          hintText: "Ex.: http://server.to.test",
+                          helperStyle: TextStyle(
+                            fontSize: 18.0,
+                          ),
+                          hintStyle: TextStyle(
+                            fontStyle: FontStyle.italic,
+                            color: Colors.white24,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(20.0),
+                    child: !_isBusy
+                        ? RaisedButton(
+                            color: Theme.of(context).accentColor,
+                            textColor: Colors.black,
+                            child: Text('VERIFY'),
+                            onPressed:
+                                _canVerify ? () => _verifyEndpoint() : null,
+                          )
+                        : CircularProgressIndicator(),
+                  )
+                ],
+              ),
+            ),
+            Flexible(
+              child: Text(_message),
             ),
           ],
         ),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
     );
+  }
+
+  _handleOnChanged(String value) {
+    setState(() {
+      _canVerify = value.isNotEmpty;
+    });
+  }
+
+  _verifyEndpoint() {
+    final URL = _myController.text;
+    setState(() {
+      _isBusy = true;
+    });
+    http.get(URL).then((response) {
+      final u = SpeechSynthesisUtterance();
+      u.lang = 'en-US';
+      u.rate = 1.0;
+      _isBusy = false;
+      if (response.body == 'Not Found') {
+        _message = "Sorry: can't resolve this endpoint";
+      } else {
+        _message = response.statusCode != null
+            ? 'This server seems to allow cross origin requests'
+            : 'This server seems to deny cross origin requests';
+      }
+      setState(() {});
+      u.text = _message;
+      window.speechSynthesis.speak(u);
+    }).catchError((error) {
+      final u = SpeechSynthesisUtterance();
+      u.lang = 'en-US';
+      u.rate = 1.0;
+      _isBusy = false;
+      switch (error.toString()) {
+        case 'XMLHttpRequest error.':
+          _message = 'This server seems to deny cross origin requests';
+          break;
+        default:
+          _message = 'There was an unexpected error';
+      }
+      setState(() {});
+      u.text = _message;
+      window.speechSynthesis.speak(u);
+    });
   }
 }
